5023369d6f6b7c95e47fa39f4bf8469cf0cab438
Lukas Lieb <lukas.lieb@axonivy.com>
2024-05-29 15:04:47 +0200

    cool vite config shit

--- app/data/engine-api.ts
+++ app/data/engine-api.ts
@@ -1,15 +1,5 @@
-const headers: HeadersInit = { 'X-Requested-With': 'neo', Authorization: btoa('Basic Developer:Developer') };
-
-const BASE = 'http://localhost:3000/';
-// const BASE = 'http://localhost:8081/api/web-ide/';
-// const BASE = 'http://192.168.3.88:8082/api/web-ide/';
+const BASE = '/api/web-ide/';
 
 export const get = (url: string) => {
-  // const r = new Request(`${BASE}${url}`);
-  // r.headers.append('X-Requested-By', 'neo');
-  // const h = new Headers();
-  // h.append('X-Requested-By', 'neo');
-  // h.append('Authorization', btoa('Developer:Developer'));
-  // h.append('Origin', 'http://localhost');
-  return fetch(`${BASE}${url}`, { headers });
+  return fetch(`${BASE}${url}`);
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        auth: 'Developer:Developer'
+      }
+    }
+  },
   plugins: [
     remix({
       ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
+        v3_throwAbortReason: true
+      }
     }),
-    tsconfigPaths(),
-  ],
+    tsconfigPaths()
+  ]
 });
