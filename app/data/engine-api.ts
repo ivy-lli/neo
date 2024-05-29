54898b38777fc78b169e5842f32b41d784811c60
Lukas Lieb <lukas.lieb@axonivy.com>
2024-05-29 14:44:42 +0200

    wip

--- app/data/engine-api.ts
+++ app/data/engine-api.ts
@@ -1,8 +1,8 @@
 const headers: HeadersInit = { 'X-Requested-With': 'neo', Authorization: btoa('Basic Developer:Developer') };
 
-// const BASE = 'http://localhost:3000/';
+const BASE = 'http://localhost:3000/';
 // const BASE = 'http://localhost:8081/api/web-ide/';
-const BASE = 'http://192.168.3.88:8082/api/web-ide/';
+// const BASE = 'http://192.168.3.88:8082/api/web-ide/';
 
 export const get = (url: string) => {
   // const r = new Request(`${BASE}${url}`);
@@ -11,5 +11,5 @@ export const get = (url: string) => {
   // h.append('X-Requested-By', 'neo');
   // h.append('Authorization', btoa('Developer:Developer'));
   // h.append('Origin', 'http://localhost');
-  return fetch(`${BASE}${url}`, { headers, credentials: 'include' });
+  return fetch(`${BASE}${url}`, { headers });
 };
--- app/routes/processes.tsx
+++ app/routes/processes.tsx
@@ -1,9 +1,27 @@
-import { Flex, SearchInput } from '@axonivy/ui-components';
+import { Flex, IvyIcon, SearchInput } from '@axonivy/ui-components';
+import { IvyIcons } from '@axonivy/ui-icons';
 import type { MetaFunction } from '@remix-run/node';
 import { useProcesses } from '~/data/processes';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Axon Ivy Processes' }, { name: 'description', content: 'Axon Ivy Processes Overview' }];
+};
+
+const ProcessCard = ({ name }: { name: string }) => {
+  return (
+    <Flex
+      direction='column'
+      justifyContent='space-between'
+      gap={2}
+      style={{ background: 'var(--N75)', padding: 'var(--size-2)', borderRadius: 10, height: 150, flex: '0 1 200px' }}
+    >
+      <div style={{ background: 'var(--background)', borderRadius: 8, flex: '1 0 auto' }} />
+      <Flex alignItems='center' justifyContent='space-between'>
+        <span>{name}</span>
+        <IvyIcon icon={IvyIcons.ArrowRight} />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default function Index() {
@@ -12,11 +30,11 @@ export default function Index() {
     <Flex direction='column' gap={4}>
       <span style={{ fontWeight: 600, fontSize: 16 }}>Processes</span>
       <SearchInput />
-      <ul>
+      <Flex gap={4} style={{ flexWrap: 'wrap' }}>
         {processes.map(process => (
-          <li key={process.name}>{process.name}</li>
+          <ProcessCard key={process.name} name={process.name} />
         ))}
-      </ul>
+      </Flex>
     </Flex>
   );
 }
