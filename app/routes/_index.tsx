412cafc9560f360ba96d525346ae8511dce6378e
Lukas Lieb <lukas.lieb@axonivy.com>
2024-05-29 08:22:41 +0200

    use button to navigate to processes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,20 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { IvyIcons } from '@axonivy/ui-icons';
+import { Button } from '@axonivy/ui-components';
+import type { MetaFunction } from '@remix-run/node';
+import { useNavigate } from '@remix-run/react';
+import '@axonivy/ui-icons/lib/ivy-icons.css';
+import '@axonivy/ui-components/lib/style.css';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
-  ];
+  return [{ title: 'New Remix SPA' }, { name: 'description', content: 'Welcome to Remix (SPA Mode)!' }];
 };
 
 export default function Index() {
+  const navigate = useNavigate();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
       <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <Link to="processes" rel="noreferrer">
-            Processes
-          </Link>
-        </li>
-      </ul>
+      <Button icon={IvyIcons.Process} size='large' onClick={() => navigate('processes')} />
     </div>
   );
 }
